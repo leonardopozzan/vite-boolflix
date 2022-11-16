689b29201713a776e08be47e554baeeeb4499bcb
leonardopozzan <leonardo.lp.pozzan@gmail.com>
2022-11-16 14:49:02 +0100

    scaffolding

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import './assets/general.scss'
+import '../node_modules/bootstrap/scss/bootstrap.scss'
+
+createApp(App).mount('#app')
